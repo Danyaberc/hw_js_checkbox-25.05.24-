6bb355dc06cf49a6b8cf66fd2b8c2c77943a7617
Danyaberc <berimecdanil963@gmail.com>
2024-05-26 17:17:46 +0300

    work hw

--- scripts/main.js
+++ scripts/main.js
@@ -9,35 +9,70 @@ let msgDone = document.querySelector('.message__done')
 // ---------------------------
 
 // INPUTS//-------------------------------
-let nameUser = document.querySelector('.username')
-let surName = document.querySelector('.surusername')
-let dateBirth = document.querySelector('.birthday')
-let adress = document.querySelector('.location')
-// let select = document.querySelector('.gender--select')
-let checkbox = document.querySelector('.agreesoft')
+// let nameUser = document.querySelector('.username')
+// let surName = document.querySelector('.surusername')
+// let dateBirth = document.querySelector('.birthday')
+// let adress = document.querySelector('.location')
+// // let select = document.querySelector('.gender--select')
+let onCheckbox = document.querySelector('#agreeuser')
+console.log(onCheckbox.checked)
 //------------------------------------------
-let checkDone = true;
 
 
-let arr= [];
+// let arr= [];
 
-let inputsArr = [nameUser,surName,dateBirth,adress,checkbox]
-console.log('inputs',inputsArr)
+// let inputsArr = [nameUser,surName,dateBirth,adress,checkbox]
+// console.log('inputs',inputsArr)
 
 formEl.addEventListener('submit',(ev) =>{
-    
     ev.preventDefault();
-    let valueEl = ev.target.value;
-    console.log('!!!!', 3 + 3)
     
-    inputsArr.forEach(input =>{
-        if(!input.valueEl === ""){
-            msgDone.innerHTML = 'Оновлено'
-        }
-        else{
-            msgEr.innerHTML = 'Заповни дані'
+    const formData = new FormData(ev.target);
+    // const info = `${formData.get('firstName')} ${formData.get('surname')} ${formData.get('datebirth')} ${formData.get('adress')} ${formData.get('agree__user')}`;
+    let infoArr = [
+        formData.get('firstName'),
+        formData.get('surname'),
+        formData.get('datebirth'),
+        formData.get('adress'),
+    ];
+
+    let checkDone = true;
+
+    let checkInp = infoArr.forEach(input => {
+        if(input === null || input.trim() === "" || input === 0 || onCheckbox.checked === false){
+            checkDone = false;
+            msgEr.innerHTML = 'Онови дані ухилянт'
+            msgDone.innerHTML = ''
+        }else if(checkDone && onCheckbox.checked === true){
+            msgDone.innerHTML = 'Дані оновлено'
+            msgEr.innerHTML = ''
+            console.log(onCheckbox.checked)
         }
     })
+
+
+    // let newInfo = info.split('')
+    // console.log(newInfo)
+
+    // let newInfoArr = newInfo.map(input => input.value);
+    // console.log(newInfoArr)
+
+
+    // if(newInfoArr.innerHTML === "" || 0 || null){
+    //     msgEr.innerHTML = 'Онови дані ухилянт'
+    // }else{
+    //     msgDone.innerHTML = 'Дані оновлено'
+    // }
+    
+    // info.forEach(input =>{
+    //     if(input === ""){
+    //         msgEr.innerHTML = 'Онови дані ухилянт'
+    //     }else{
+    //         msgDone.innerHTML = 'Дані оновлено'
+    //     }
+    // })
+    // msgDone.innerHTML = info;
+
 
 });
 
